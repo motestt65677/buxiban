de1b950fcfce8704f58f23fc20405b499cac5413
motestt65677 <pkpk99887766@gmail.com>
2018-06-28 22:54:25 +0800

    added course CRUD

---
+++ db/migrate/20180627121419_create_student_course.rb
@@ -0,0 +1,9 @@
+class CreateStudentCourse < ActiveRecord::Migration[5.1]
+  def change
+    create_table :student_courses do |t|
+      t.belongs_to :student, foreign_key: true
+      t.belongs_to :course, foreign_key: true
+      t.string :paystatus
+    end
+  end
+end
